45945b846da4be2d91adac380ee048e4eedef9cf
djdaantruong <daantd.a12017@gmail.com>
2020-11-04 16:49:33 +0700

    hash password with bcrypt and login with jsonwebtoken

--- index.js
+++ index.js
@@ -27,13 +27,48 @@ app.get('/', (req, res)=> {
 app.post('/api/users/register', (req, res) => {
     const user = new User(req.body);
 
-    user.save((err, userDate) => {
+    user.save((err, doc) => {
         if (err) return res.json({ success: false, err});
         return res.status('200').json({ 
-            success: true
+            success: true,
+            userData: doc
         });
     });
     
 });
+
+app.post('/api/user/login', (req, res) => {
+  //find the email
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user)
+      return res.json({
+        loginSuccess: false,
+        message: "Auth falsed, email not found" 
+      });
+      //comparePassword
+
+      user.comparePassword(req.body.password, (err, isMatch) => {
+        if (!isMatch) {
+          return res.json({ loginSuccess: false, message: "wrong password"});
+        }
+      })
+
+      //ganerateToken
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+        res.cookie("x_auth", user.token)
+           .status(200)
+           .json({
+             loginSuccess: true
+           })
+      })
+
+  })
+
+
+
+})
+
+
 
 app.listen(5000);
