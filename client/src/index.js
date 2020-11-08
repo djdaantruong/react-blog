935fa69154cbcf486608c8bf7af6c1a9ed736beb
djdaantruong <daantd.a12017@gmail.com>
2020-11-08 20:25:33 +0700

    setup redux

--- client/src/index.js
+++ client/src/index.js
@@ -5,12 +5,23 @@ import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route, Link } from "react-router-dom";
 
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import promiseMiddleware from "redux-promise";
+import ReduxThunk from "redux-thunk";
+import Reducer from "./reducers";
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
 
 ReactDOM.render(
+
+  <Provider store={ createStoreWithMiddleware(Reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()) }>
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
+
 );
 
 // If you want to start measuring performance in your app, pass a function
